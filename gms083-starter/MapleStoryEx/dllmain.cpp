d3697411f7c74899c6542a5f70309fbc59b720cd
“unsafeblackcat” <unsafeblackcat@gmail.com>
2023-11-22 01:21:30 +0800

    可以尝试做技能宏和自动吃药的逻辑了。。

--- gms083-starter/MapleStoryEx/dllmain.cpp
+++ gms083-starter/MapleStoryEx/dllmain.cpp
@@ -31,6 +31,12 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+typedef int(__fastcall* pfun_skills)(void* pthis, int dummy, int id, int param1, int param2);
+extern pfun_skills g_skills;
+extern int key;
+
+#include "memory.h"
+
 unsigned __stdcall start_work(LPVOID lpParam)
 { 
     CGlobal::pins()->init();
@@ -56,6 +62,34 @@ unsigned __stdcall start_work(LPVOID lpParam)
     do 
     {
         Sleep(1000);
+
+
+        //外部技能调用，通过键盘回调得到按键参数通过全局变量来搜索到按键ID
+
+        int key_pos = 0x1E;
+
+        int* value = 0;
+        memory::read(0x00BED5A0, (int&)value);
+        if (value != 0)
+        {
+            char* pos = (char*)(value + key_pos);
+            pos += key_pos + 4 + 1;
+            g_skills((void*)key, 0, *((int*)pos), 0, 0);
+        }
+
+        Sleep(1000);
+
+        key_pos = 0x20;
+
+        value = 0;
+        memory::read(0x00BED5A0, (int&)value);
+        if (value != 0)
+        {
+            char* pos = (char*)(value + key_pos);
+            pos += key_pos + 4 + 1;
+            g_skills((void*)key, 0, *((int*)pos), 0, 0);
+        }
+
     } while (true);
     return 0;
 }
