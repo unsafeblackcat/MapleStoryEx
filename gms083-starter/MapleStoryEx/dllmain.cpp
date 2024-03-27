d1d94eea35a725a80728c88d395554519b8cb56c
unsafeblackcat <unsafeblackcat@gmail.com>
2024-03-28 01:01:15 +0800

    调整下逻辑

--- gms083-starter/MapleStoryEx/dllmain.cpp
+++ gms083-starter/MapleStoryEx/dllmain.cpp
@@ -21,8 +21,9 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH: 
-    {
-        _beginthreadex(nullptr, 0, start_work, nullptr, 0, nullptr);
+	{  
+		_beginthreadex(nullptr, 0, start_work, nullptr, 0, nullptr);
+		Sleep(3000);
     }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
@@ -38,33 +39,33 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 
 unsigned __stdcall start_work(LPVOID lpParam)
 {
-    CGlobal::pins()->init();
-    CRole::pins();
-    CFunction::pins(); 
-    CKeyboard::pins();
-    CommandInput::pins(); 
-    CPlugins::pins()->load();
+	CGlobal::pins()->init();
+	CPlugins::pins()->load();
+    //CRole::pins();
+    //CFunction::pins(); 
+    //CKeyboard::pins();
+    //CommandInput::pins();  
 
     char current_play_role_name[64] = { 0x0 };
     strcpy(current_play_role_name, CFunction::pins()->role_name());
      
-    CFunction::pins()->sendmsg("~~~~~~~~MapleStoryEx扩展插件启动!~~~~~~~~");
-    CFunction::pins()->sendmsg("~~~~~~~~!help 显示可用插件的帮助命令.~~~~~~~~");
-    CFunction::pins()->sendmsg("~~~~~~~~!show 显示所有当前已加载的插件介绍和命令.~~~~~~~~");
+//     CFunction::pins()->sendmsg("~~~~~~~~MapleStoryEx扩展插件启动!~~~~~~~~");
+//     CFunction::pins()->sendmsg("~~~~~~~~!help 显示可用插件的帮助命令.~~~~~~~~");
+//     CFunction::pins()->sendmsg("~~~~~~~~!show 显示所有当前已加载的插件介绍和命令.~~~~~~~~");
      
     do 
     {
         Sleep(1000); 
-        const char* play_role_name = CFunction::pins()->role_name();
-        if ((play_role_name != nullptr) && (strlen(play_role_name) != 0))
-        {
-            if (strcmp(current_play_role_name, play_role_name) != 0)
-            {
-                //玩家切换了角色
-                CPlugins::pins()->replay_role();
-                strcpy(current_play_role_name, CFunction::pins()->role_name());
-            }
-        }
+//         const char* play_role_name = CFunction::pins()->role_name();
+//         if ((play_role_name != nullptr) && (strlen(play_role_name) != 0))
+//         {
+//             if (strcmp(current_play_role_name, play_role_name) != 0)
+//             {
+//                 //玩家切换了角色
+//                 CPlugins::pins()->replay_role();
+//                 strcpy(current_play_role_name, CFunction::pins()->role_name());
+//             }
+//         }
     } while (true);
     return 0;
 } 
